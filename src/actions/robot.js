d70487cddbcdd9baf43e4c1670dc51d1e49e5ac0
Stanley Nguyen <hung.ngn.the@gmail.com>
2018-06-25 00:44:52 +0800

    WIP Robot Add

--- src/actions/robot.js
+++ src/actions/robot.js
@@ -1,5 +1,5 @@
 import {
-  // ROBOT_ADD,
+  ROBOT_ADD,
   // ROBOT_DELETE,
   // ROBOT_EDIT,
   ROBOT_FAIL,
@@ -7,14 +7,14 @@ import {
   ROBOT_SUCCESS,
   ROBOT_TOGGLE_EDIT,
 } from 'constants/actionTypes';
-import { getAllRobots } from 'lib/robotService';
+import { getAllRobots, createNewRobot } from 'lib/robotService';
 import { tokenClear } from './token';
 import { withAuth } from 'lib/misc';
 
 const robotLoading = () => ({ type: ROBOT_LOADING });
 const robotSuccess = payload => ({ type: ROBOT_SUCCESS, payload });
 const robotFail = payload => ({ type: ROBOT_FAIL, payload });
-// const robotAdd = payload => ({ type: ROBOT_ADD, payload });
+const robotAdd = payload => ({ type: ROBOT_ADD, payload });
 // const robotEdit = payload => ({ type: ROBOT_EDIT, payload });
 // const robotDelete = payload => ({ type: ROBOT_DELETE, payload });
 
@@ -30,6 +30,18 @@ export const fetchAllRobots = () => async (dispatchEvent, getState) => {
   try {
     const { robots } = await withAuth(getAllRobots(token.value), dispatchFunc);
     dispatchEvent(robotSuccess(robots));
+  } catch (e) {
+    dispatchEvent(robotFail(e));
+  }
+};
+
+export const addNewRobot = rb => async (dispatchEvent, getState) => {
+  const { token } = getState();
+  const dispatchFunc = dispatchEvent.bind(null, tokenClear);
+  dispatchEvent(robotLoading());
+  try {
+    const robot = await withAuth(createNewRobot(rb, token.value), dispatchFunc);
+    dispatchEvent(robotAdd(robot));
   } catch (e) {
     dispatchEvent(robotFail(e));
   }
--- src/lib/robotService.js
+++ src/lib/robotService.js
@@ -10,3 +10,29 @@ export const getAllRobots = async token =>
       headers: { Authorization: token },
     }),
   );
+
+export const createNewRobot = async (robot, token) =>
+  await serviceTemplate(
+    fetch(`${SERVICE_URL}`, {
+      method: 'POST',
+      headers: { Authorization: token, 'Content-Type': 'application/json' },
+      body: JSON.stringify(robot),
+    }),
+  );
+
+export const editRobot = async (robot, token) =>
+  await serviceTemplate(
+    fetch(`${SERVICE_URL}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json', Authorization: token },
+      body: JSON.stringify(robot),
+    }),
+  );
+
+export const deleteRobot = async ({ id }, token) =>
+  await serviceTemplate(
+    fetch(`${SERVICE_URL}/${id}`, {
+      method: 'DELETE',
+      headers: { Authorization: token },
+    }),
+  );
