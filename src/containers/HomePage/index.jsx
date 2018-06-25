f3c17db07fbe6815eb57570621aadfa6d046f69c
Stanley Nguyen <hung.ngn.the@gmail.com>
2018-06-25 14:11:31 +0800

    WIP User home page

--- src/containers/HomePage/index.jsx
+++ src/containers/HomePage/index.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Container, Grid, Image, Button, Icon } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
-const StyleWrapper = styled.div``;
+import logo from 'assets/logo.svg';
+
+const StyleWrapper = styled(Container)``;
 
 export default class HomePage extends React.Component {
   render() {
-    return <StyleWrapper>user home page</StyleWrapper>;
+    return (
+      <StyleWrapper>
+        <Grid columns="equal">
+          <Grid.Row centered>
+            <Image src={logo} />
+          </Grid.Row>
+          <Grid.Row centered>
+            <h2>Welcome to do-do Cloud</h2>
+          </Grid.Row>
+          <Grid.Row centered>
+            <h4>Please choose to proceed with one of the services</h4>
+          </Grid.Row>
+          <Grid.Row centered>
+            <Button basic color="orange" as={Link} to="/parcels">
+              <Icon name="archive" />
+              Parcels Management
+            </Button>
+          </Grid.Row>
+        </Grid>
+      </StyleWrapper>
+    );
   }
 }
